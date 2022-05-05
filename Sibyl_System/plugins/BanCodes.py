c01d4989db4084f9a8d762ed686a835795bbdac4
Pachirisu <97147352+ThePachirisu@users.noreply.github.com>
2022-05-05 14:13:30 +0530

    Update BanCodes.py

--- Sibyl_System/plugins/BanCodes.py
+++ Sibyl_System/plugins/BanCodes.py
@@ -1,24 +1,24 @@
-help_plus = """ Here is Help for Bancodes  :-
-֍ • {OSx00} - Scammer 
-֍ • {OSx01} - SPAM ADDING MEMBER
-֍ • {OSx02} - CHILD ABUSE
-֍ • {OSx03} - ILLEGAL
-֍ • {OSx04} - FRAUD PROMOTION  [ANY KIND]
-֍ • {OSx05} - PHISHING
-֍ • {OSx06} - BAN EVASION
-֍ • {OSx07} - RAID/SPAM INFLAMED
-֍ • {OSx08} - ADDING SPAMBOTS
-֍ • {OSx09} - KRIMINALANT
-֍ • {OSx10} - SCAMMER
-֍ • {OSx11} - ABUSE SPAM
-֍ • {OSx12} - IMPERSONATION
-֍ • {OSx13} - MD/BTC SCAM
-֍ • {OSx14} - RAID INITIALIZER
-֍ • {OSx15} - RAID PARTICIPANT
-֍ • {OSx16} - SPAMBOT
-֍ • {OSx17} - CYBER THREATENING / CYBER BULLY
-֍ • {OSx18} - NSFW SPAMMER
+help_plus = """ Here is Help for Slyveon Bancodes  :-
+🌸 • {SXSx00} - 𝘚𝘤𝘢𝘮𝘮𝘦𝘳
+🌸 • {SXSx01} - 𝘚𝘱𝘢𝘮 𝘔𝘦𝘮𝘣𝘦𝘳 𝘢𝘥𝘥𝘪𝘯𝘨
+🌸 • {SXSx02} - 𝘊𝘩𝘪𝘭𝘥 𝘈𝘣𝘶𝘴𝘦
+🌸 • {SXSx03} - 𝘐𝘭𝘭𝘦𝘨𝘢𝘭
+🌸 • {SXSx04} - 𝘗𝘳𝘰𝘮𝘰𝘵𝘪𝘰𝘯 𝘰𝘧  𝘈𝘯𝘺 𝘒𝘪𝘯𝘥
+🌸 • {SXSx05} - 𝘗𝘩𝘪𝘴𝘩𝘪𝘯𝘨
+🌸 • {SXSx06} - 𝘉𝘢𝘯 𝘌𝘴𝘤𝘭𝘢𝘷𝘢𝘵𝘪𝘰𝘯
+🌸 • {SXSx07} - 𝘙𝘢𝘪𝘥/𝘚𝘱𝘢𝘮 𝘐𝘯𝘧𝘭𝘢𝘮𝘦𝘥
+🌸 • {SXSx08} - 𝘈𝘥𝘥𝘪𝘯𝘨 𝘚𝘱𝘢𝘮𝘣𝘰𝘵𝘴
+🌸 • {SXSx09} - 𝘚𝘱𝘦𝘤𝘪𝘢𝘭 𝘵𝘩𝘪𝘯𝘨 
+🌸 • {SXSx10} - 𝘚𝘱𝘢𝘮𝘮𝘦𝘳
+🌸 • {SXSx11} - 𝘚𝘱𝘢𝘮 𝘈𝘣𝘶𝘴𝘦𝘪𝘷𝘦
+🌸 • {SXSx12} - 𝘐𝘮𝘱𝘦𝘳𝘢𝘰𝘯𝘪𝘵𝘢𝘪𝘰𝘯
+🌸 • {SXSx13} - 𝘔𝘥/𝘉𝘵𝘤  𝘚𝘤𝘢𝘮
+🌸 • {SXSx14} - 𝘙𝘢𝘪𝘥 𝘪𝘯𝘴𝘵𝘢𝘭𝘭𝘢𝘵𝘪𝘰𝘯
+🌸 • {SXSx15} - 𝘙𝘢𝘪𝘥 𝘱𝘢𝘳𝘵𝘪𝘤𝘪𝘱𝘢𝘯𝘵
+🌸 • {SXSx16} - 𝘚𝘱𝘢𝘮 𝘣𝘰𝘵
+🌸 • {SXSx17} - 𝘊𝘺𝘣𝘦𝘳 𝘛𝘩𝘳𝘦𝘢𝘵𝘪𝘯𝘨 / 𝘊𝘺𝘣𝘦𝘳 𝘣𝘶𝘭𝘭𝘺
+🌸 • {SXSx18} - 𝘕𝘍𝘚𝘞 𝘴𝘱𝘢𝘮𝘮𝘦𝘳
 
-Powered By  Slyveon Support
+𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆 𝗦𝗹𝘆𝘃𝗲𝗼𝗻 𝗧𝗲𝗮𝗺 
 """
 __plugin_name__ = "BanCodes" 
